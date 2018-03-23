5e3e94fe6f6f6064e5999a1a95651bff5bdfd9ee
Forever <lynk0613@hotmail.com>
2018-03-23 07:55:45 -0400

    fix typo

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -6,3 +6,6 @@ export { default as BackToTop }         from './backToTop/BackToTop';
 export { default as ErrorAlert }        from './alert/ErrorAlert';
 export { default as WarningAlert }      from './alert/WarningAlert';
 export { default as Alert }             from './alert/Alert';
+export { default as Dashboard }         from './adminPanel/Dashboard';
+export { default as ManageCategories }  from './adminPanel/ManageCategories';
+export { default as ManageProducts }    from './adminPanel/ManageProducts'; 

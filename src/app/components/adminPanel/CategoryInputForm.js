ffc5f5ea6a08638bb57d089ecb02987b481e5bd9
Forever <lynk0613@hotmail.com>
2018-03-23 19:48:21 -0400

    CURD

---
+++ src/app/components/adminPanel/CategoryInputForm.js
@@ -0,0 +1,37 @@
+// @flow weak
+
+import React, {
+  PureComponent
+}                 from 'react';
+import PropTypes  from 'prop-types';
+ 
+
+
+class CategoryInputForm extends PureComponent {
+  render() {
+    return (
+      <div>
+        <div className="form-group">
+          <label htmlFor="categoryName"> Category Name </label>
+          <input type="text" className="form-control" id="categoryName" placeholder="Category Name"/>
+        </div> 
+        <div className="form-group">
+          <label htmlFor="categoryDescription"> Description </label>
+          <input type="text" className="form-control" id="categoryDescription" placeholder="Description"/>
+        </div> 
+        <div className="form-group">
+          <label htmlFor="categoryActive"> isActive </label>
+          <input type="checkbox" className="form-control" id="categoryActive" placeholder="isActive"/>
+        </div> 
+        <div className="form-group">
+          <label htmlFor="categorySort"> Sort </label>
+          <input type="text" className="form-control" id="categorySort" placeholder="Sort"/>
+        </div> 
+      </div>
+    );
+  }
+}
+
+export default CategoryInputForm;
+
+
--- src/app/components/adminPanel/ManageCategories.js
+++ src/app/components/adminPanel/ManageCategories.js
@@ -2,26 +2,212 @@
 
 import React, {
   PureComponent
-}                 from 'react';
-import PropTypes  from 'prop-types';
+}                   from 'react';
+import PropTypes    from 'prop-types';
+import gql          from 'graphql-tag';
+import { graphql }  from 'react-apollo';
+import { compose }  from 'react-apollo'
+import CategoryInputForm from './CategoryInputForm'
 
-import {
-  Motion,
-  spring,
-  presets
-}                 from 'react-motion';
+const ALL_CATEGORIES = gql`
+  query GetAllCategories($first: Int, $after: String, $orderBy: [CategoriesOrderByArgs]) {
+    viewer {
+        allCategories(first: $first, after: $after, orderBy: $orderBy) {
+          edges {
+            cursor
+            node {
+              id
+              name
+              description
+              parentID
+              isActive
+              sort
+              createdAt
+              modifiedAt
+            }
+          }
+        }
+      }
+    }
+`;
+
+const CREATE_CATEGORY = gql`
+  mutation createCategory($category: CreateCategoriesInput!) {
+    createCategories(input: $category) {
+      changedCategories{
+        name
+        description
+        parentID
+        isActive
+        sort
+      }
+    } 
+  }
+`
+const UPDATE_CATEGORY = gql`
+  mutation UpdateCategory($category: UpdateCategoriesInput!) {
+    updateCategories(input: $category) {
+      changedCategories {
+        id
+        name
+        description
+        parentID
+        isActive
+        sort
+      }
+    }
+  }
+`
+
+const DELETE_CATEGORY = gql`
+  mutation DeleteCategory($category: DeleteCategoriesInput!) {
+    deleteCategories(input: $category) {
+      changedCategories {
+        id
+      }
+    }
+  }
+`
+
+const querySet = compose (
+  graphql(ALL_CATEGORIES, {name: "allCategories"}),
+  graphql(CREATE_CATEGORY, {name: "createCategory"}),
+  graphql(UPDATE_CATEGORY, {name: "updateCategory"}),
+  graphql(DELETE_CATEGORY, {name: "deleteCategory"}),
+)
 
 
 class ManageCategories extends PureComponent {
 
+  
+ 
+
+  state = {
+    selectedCategoryIndex: null,
+  };
+
+  componentDidMount() {
+
+  }
+
+  selectCategory(index) {
+    this.setState({	
+			selectedCategoryIndex: index
+    });
+    
+    document.getElementById('categoryName').value = this.props.allCategories.viewer.allCategories.edges[index].node.name;
+    document.getElementById('categoryDescription').value = this.props.allCategories.viewer.allCategories.edges[index].node.description;
+    document.getElementById('categoryActive').value = this.props.allCategories.viewer.allCategories.edges[index].node.isActive;
+    document.getElementById('categorySort').value = this.props.allCategories.viewer.allCategories.edges[index].node.sort;
+  }
+
+  //CREATE
+  createCategory=async () => {
+    await this.props.createCategory({
+      variables: {
+        "category": 
+        {
+          "name": document.getElementById('categoryName').value  ,
+          "description": document.getElementById('categoryDescription').value  ,
+          "isActive": document.getElementById('categoryActive').value , 
+          "parentID": "",
+          "sort": parseInt(document.getElementById('categorySort').value)
+        }
+      }
+    });
+  }
+
+  //UPDATE
+  updateCategory=async () => {
+    const index = this.state.selectedCategoryIndex;
+    if (index == null) {
+      alert('Please select one of categories');
+      return;
+    }
+    const categoryID = this.props.allCategories.viewer.allCategories.edges[index].node.id;
+
+    await this.props.updateCategory({
+      variables: {
+        "category": 
+        {
+          "id": categoryID,
+          "name": document.getElementById('categoryName').value  ,
+          "description": document.getElementById('categoryDescription').value  ,
+          "isActive": document.getElementById('categoryActive').value , 
+          "parentID": "",
+          "sort": parseInt(document.getElementById('categorySort').value)
+        }
+      }
+    });
+  }
+
+  //DELETE
+  deleteCategory=async () => {
+    const index = this.state.selectedCategoryIndex;
+    if (index == null) {
+      alert('Please select one of categories');
+      return;
+    }
+    const categoryID = this.props.allCategories.viewer.allCategories.edges[index].node.id;
+
+    await this.props.deleteCategory({
+      variables: {
+        "category": 
+        {
+          "id": categoryID,
+        }
+      }
+    });
+  }
+  
 
   render() {
+
+    if (this.props.allCategories && this.props.allCategories.loading) {
+      return <div>Loading ...</div>;
+    }
+
+    if (this.props.allCategories && this.props.allCategories.error) {
+      console.error(this.props.allCategories.error);
+      return <div>Error occurred</div>;
+    }
+
+    const allCategories = this.props.allCategories.viewer.allCategories.edges;
+    if (allCategories.length === 0) {
+      return <div>No Category</div>;
+    }
+
+    const selectedCategoryIndex = this.state.selectedCategoryIndex 
+
     return (
-      <div>
-        ManageCategories
+      <div className="row">
+        <div className="col-md-4">
+          <h2>Categories</h2>
+          <ul>
+          {
+            allCategories.map((item, index) => (
+              <li>
+                <a onClick={this.selectCategory.bind(this, index)}>
+                  {item.node.name}
+                </a>
+              </li>
+            ))
+          }
+          </ul>
+        </div>
+        <div className="col-md-8">
+          <h2>{selectedCategoryIndex!=null&&allCategories[selectedCategoryIndex].node.name}</h2>
+          <CategoryInputForm/>
+          <button type="button" className="btn btn-primary" onClick={this.createCategory}>Create</button>
+          <button type="button" className="btn btn-primary" onClick={this.updateCategory}>Update</button>
+          <button type="button" className="btn btn-danger"  onClick={this.deleteCategory}>Remove</button>
+          
+        </div>
       </div>
     );
   }
 }
 
-export default ManageCategories;
+export default querySet(ManageCategories)
+//export default graphql(getAllCategories, {name: "allCategories"}) (ManageCategories)
+

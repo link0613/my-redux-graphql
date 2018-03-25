a3f276cc80214bf51091faa260fe8a3d238d6d96
Forever <lynk0613@hotmail.com>
2018-03-25 10:44:25 -0400

    productStyle

--- src/app/components/product/ProductView.js
+++ src/app/components/product/ProductView.js
@@ -10,23 +10,20 @@ class ProductView extends PureComponent {
 
   render() {
     const {productList} = this.props;
-    console.log(productList);
     return(
-      <div className="row">
-        <div className="col-lg-10 offset-lg-2">
-          {
-            productList.map((item, index) => (
-              <div key={index} className="product-thumb h4">
-                <div className="product-thumb-name">
-                  {item.name}
-                </div>
-                <div className="product-thumb-price">
-                  ${item.price}
-                </div>              
+      <div className="product">
+        {
+          productList.map((item, index) => (
+            <div key={index} className="product-thumb h4">
+              <div className="product-thumb-name">
+                {item.name}
               </div>
-            ))
-          }
-        </div>
+              <div className="product-thumb-price">
+                ${item.price}
+              </div>              
+            </div>
+          ))
+        }
       </div>
     );
   }
